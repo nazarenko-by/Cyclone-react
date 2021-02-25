78dcc1c3a405e164d6cbf3f5ebe8565aa9508e05
nazarenko-by <bodean56@ukr.net>
2021-02-25 13:39:24 +0200

    added HotTopicList

---
+++ src/App/HotTopics/HotTopicList/HotTopicList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HotTopicList = () => {
+    return (
+        <div class="topics-list">
+
+        </div>
+    )
+}
+
+export default HotTopicList
--- src/App/HotTopics/HotTopics.js
+++ src/App/HotTopics/HotTopics.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
+import HotTopicList from './HotTopicList/HotTopicList'
+
 const HotTopics = () => {
     return (
         <div class="hot-topics">
             <div class="big-title">Hot <span class="text-line">Topics</span></div>
+            <HotTopicList/>
         </div>)
 }
 
