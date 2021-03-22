ba689b92f437a5d3c51d3b6038ef65fad499a5fe
nazarenko-by <bodean56@ukr.net>
2021-03-22 16:03:30 +0200

    added like button

--- src/Components/CommentShareButton/CommentShareButton.js
+++ src/Components/CommentShareButton/CommentShareButton.js
@@ -3,14 +3,18 @@ import ModalWidow from '../ModalWindow/ModalWindow'
 
 import commentImg from './images/comments.png'
 import shareImg from './images/share.png'
+import likeImg from './images/like.png'
 
 const CommentShareButton = ({id,modalPositiom}) =>{
     const [openCloseModal, setOpenCloseModal] = useState({visibility: "hidden", opacity:"0"})
     return(
         <>
             <div className="comment-share-button unselectable">
-                <button className="comment" onClick={()=>setOpenCloseModal({visibility: "visible", opacity:"1"})}><img src={commentImg} alt=""/></button>
+                <button className="like"><img src={likeImg} alt=""/></button>
                 <button className="share"><img src={shareImg} alt=""/></button>
+                <button className="comment" onClick={()=>setOpenCloseModal({visibility: "visible", opacity:"1"})}><img src={commentImg} alt=""/></button>
+                
+                
             </div>
             <ModalWidow 
                 id = {id} 
