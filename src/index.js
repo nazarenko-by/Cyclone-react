6c9e09d3ad888dff32911db344aa0c9bc87e8443
nazarenko-by <bodean56@ukr.net>
2021-03-22 15:43:37 +0200

    added reduser

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import {BrowserRouter} from 'react-router-dom'
+import { createStore } from 'redux';
 import App from './App/App'
+import { likeReducer } from './redux/likeReducer';
+
+const store = createStore(
+    likeReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>,document.getElementById("root"))
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>,document.getElementById("root"))
---
+++ src/redux/likeReducer.js
@@ -0,0 +1,16 @@
+export const likeReducer = (state, action) => {
+    switch (action.type) {
+      case "LIKE":
+        return {
+          ...state,
+          [action.id]: true
+        };
+      case "DISLIKE":
+        return {
+          ...state,
+          [action.id]: false
+        };
+      default:
+        return state;
+    }
+};
