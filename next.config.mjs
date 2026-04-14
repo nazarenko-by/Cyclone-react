88f35b9068c65e1c56e95053e538335398cd9521
nazarenko-by <bodean56@ukr.net>
2026-04-14 20:25:15 +0300

    cyclone-react: add deploy settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    devIndicators: false,
-    reactStrictMode: true,
+    output: 'export',
+    basePath: '/Cyclone-react',
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
