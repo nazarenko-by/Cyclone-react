62d5dfa4d3c69ceb45c0b34a6d650e6788c0ac98
nazarenko-by <bodean56@ukr.net>
2026-04-14 19:10:05 +0300

    follow-us: update links

--- src/features/ui/FollowUs/index.jsx
+++ src/features/ui/FollowUs/index.jsx
@@ -7,18 +7,18 @@ import "@/styles/components/followUs.scss"
 const socialLinks = [
 	{
 		name: "Instagram",
-		url: "https://www.instagram.com",
+		url: "https://www.instagram.com/nby.frontend/",
 		icon: "/images/social/instagram.svg",
 	},
 	{
 		name: "LinkedIn",
-		url: "https://www.linkedin.com",
+		url: "https://www.linkedin.com/in/bohdan-nazarenko-by/",
 		icon: "/images/social/linkedin.svg",
 	},
 	{
-		name: "X",
-		url: "https://www.x.com",
-		icon: "/images/social/x-logo.svg",
+		name: "Threads",
+		url: "https://www.threads.com/@nby.frontend",
+		icon: "/images/social/threads.svg",
 	},
 ]
 
@@ -29,7 +29,13 @@ const FollowUs = ({ title, icons }) => {
 			<ul className="follow-us-list">
 				{socialLinks.map((link) => (
 					<li key={link.name}>
-						<Link className="follow-us-link" href={link.url} target="_blank" rel="noopener noreferrer">
+						<Link
+							className="follow-us-link"
+							href={link.url}
+							target="_blank"
+							rel="noopener noreferrer"
+							title={link.name}
+						>
 							{icons ? <Image src={link.icon} alt={link.name} width={20} height={20} /> : link.name}
 						</Link>
 					</li>
